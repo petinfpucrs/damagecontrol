262e1bb36bc6cb75d00e76d2cb29ba7d1e6c392c
ACheinquer <a.cheinquer@edu.pucrs.br>
2022-05-19 14:47:20 -0300

    comentários no código main

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ df = pd.read_csv('data.csv')
 
 print(df.to_string()) 
 print("Damage Control!")
-
+#Programa da print nas opções
 print("Escolha um par dos seguintes componentes:")
 print("1 - Álcool")
 print("2 - Água Sanitária")
 print("3 - Amônia")
 print("4 - Alvejantes")
 
-
+#Pede o primeiro componente
 c1 = int(input("Primeiro Componente "))
 
 def Comp1(c1):
@@ -29,7 +29,7 @@ def Comp1(c1):
 
 
 
-
+#Pede o segundo
 c2 = int(input("Segundo Componente "))
 
 def Comp2(c2):
@@ -43,4 +43,5 @@ def Comp2(c2):
         case 4:
             return "Alvejantes"
 
+#Imprime os 2, mas precisamos que ele imprima o resultado da mistura utilizando o data.csv
 print(Comp1(c1), Comp2(c2))

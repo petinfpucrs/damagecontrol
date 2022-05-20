5733c1ce67d3d8acfd9d462a7ba787d65cca8f82
Vitor Machado <104864241+Machxado@users.noreply.github.com>
2022-05-20 15:34:22 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
+#DataFrame
 fonte = pd.read_csv('misturas.csv')
 df = pd.DataFrame(fonte)
 
+#Menu
 print("Damage Control!")
-#Programa da print nas opções
 print("################################")
 print("* 1 - Álcool                  *")
 print("* 2 - Água Sanitária          *")
@@ -16,8 +17,9 @@ print("* 7 - Bicarbonato de Sódio    *")
 print("* 8 - Água Oxigenada          *")
 print("* 9 - Desinfetante            *")
 print("###############################")
-c1 = int(input("Digite o número correspondente ao componente que você deseja misturar: "))
+c1 = int(input("Digite o número correspondente ao 1° componente da mistura: "))
 
+#operações para pesquisa no SGBD da composição1
 def Comp1(c1):
     match c1:
         case 1:
@@ -41,11 +43,38 @@ def Comp1(c1):
         case _ :
             return 5
 
-#entregando resultado
-Compos = Comp1(c1)
+#operações para pesquisa no SGBD da composição2
+c2 = int(input("Agora, digite o número correspondente ao 2° componente da mistura: "))
 
-if(Comp1(c1) == 5):
+def Comp2(c2):
+        match c2:
+            case 1:
+                return "Álcool"
+            case 2:
+                return "Água Sanitária"
+            case 3:
+                return "Amônia"
+            case 4:
+                return "Alvejantes"
+            case 5:
+                return "Vinagre"
+            case 6:
+                return "Detergente"
+            case 7:
+                return "Bicarbonato de Sódio"
+            case 8:
+                return "Água Oxigenada"
+            case 9:
+                return "Desinfetante"
+            case _:
+                return 5
+
+#entregando resultado
+Compos1 = Comp1(c1)
+Compos2 = Comp2(c2)
+
+if(Comp1(c1 or c2) == 5):
     print("Opção Inválida")
 
 else:
-    print(df.loc[df['composição 1'] == Compos])
+    print(df.loc[(df['composição 1'] == Compos1) & (df['composição 2'] == Compos2),['resultado']])

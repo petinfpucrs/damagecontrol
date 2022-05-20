0391b0154a3830e75909fdfd1c230069401a7197
Jair Francisco H Machado <83318854+ChicoNK@users.noreply.github.com>
2022-05-20 10:06:43 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,57 +1,4 @@
-import os
-import pandas as pd
 
-df = pd.read_csv('data.csv')
-
-print(df.to_string()) 
-print("Damage Control!")
-#Programa da print nas opções
-print("Escolha um par dos seguintes componentes:")
-print("1 - Álcool")
-print("2 - Água Sanitária")
-print("3 - Amônia")
-print("4 - Alvejantes")
-
-#Pede o primeiro componente
-c1 = int(input("Primeiro Componente "))
-
-def Comp1(c1):
-    match c1:
-        case 1:
-            return "Álcool"
-        case 2:
-            return "Água Sanitária"
-        case 3:
-            return "Amônia"
-        case 4:
-            return "Alvejantes"
-
-
-
-
-#Pede o segundo
-c2 = int(input("Segundo Componente "))
-
-def Comp2(c2):
-    match c2:
-        case 1:
-            return "Álcool"
-        case 2:
-            return "Água Sanitária"
-        case 3:
-            return "Amônia"
-        case 4:
-            return "Alvejantes"
-
-#Imprime os 2, mas precisamos que ele imprima o resultado da mistura utilizando o data.csv
-print(Comp1(c1), Comp2(c2))
-
-
-
-
-
-#As seguintes linhas são o código do Vitor
-"""
 import pandas as pd
 
 fonte = pd.read_csv('misturas.csv')
@@ -103,4 +50,3 @@ if(Comp1(c1) == 5):
 
 else:
     print(df.loc[df['composição 1'] == Compos])
-"""
